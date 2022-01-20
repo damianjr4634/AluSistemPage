5b33b1d5287333265a6fe16ccf194f96aaf3f529
damian <rdj4634@gmail.com>
2022-01-20 00:25:38 -0300

    inscrip materias

--- EsbaBlazorAppAuth/Pages/Alumno/Materias/InscripcionMateria.razor.cs
+++ EsbaBlazorAppAuth/Pages/Alumno/Materias/InscripcionMateria.razor.cs
@@ -18,11 +18,117 @@ namespace EsbaBlazorAppAuth.Pages.Alumno.Materias
 {
     public partial class InscripcionMateria : _BasePage
     {
-        [Parameter]
-        public string CarreraId { set; get; } = "";
-        [Parameter]
-        public string AlumnoId { set; get; } = "";
+ 
         [Parameter]
         public string MateriaId { set; get; } = "";
+        private bool _add;
+        private bool busy = false;
+
+        public class MateriaInscripcion
+        {
+            public string? MATERIA { get; set; }
+            public int FERRCOD { get; set; }
+            public string? FERRWEB { get; set; }           
+        }
+
+        public class Inscripcion
+        {
+            public string? Turno { get; set; }            
+        }
+
+        public Inscripcion _inscripcion = new Inscripcion();
+
+        public MateriaInscripcion? _materiaInscripcion;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            string query = "";
+
+            if (firstRender)
+            {
+                try
+                {
+                    if (appSession.UserCode == 0)
+                    {
+                        await appSession.LoadInformationUser();
+                    }
+                    _add = false;
+
+                    using (var dbContext = await appSession.DbContextCreate())
+                    {
+                        _materiaInscripcion = await dbContext.QuerySingleOrDefaultAsync<MateriaInscripcion>(@$"select FERRCOD, FERRWEB, MATERIA from ALUMNOS A, XXX_INSC_VALMAT(A.cod_alu, '{appSession.Carreras[0].Id}', '{MateriaId}', 'I') WHERE A.INDICE={appSession.UserCode}");                        
+                    }
+
+                    StateHasChanged();
+                }
+                catch (Exception err)
+                {
+                    if (err.InnerException != null && err.InnerException.Message != "")
+                    {
+                        toastService.ShowError(err.InnerException.Message);
+                    }
+                    else
+                    {
+                        toastService.ShowError(err.Message);
+                    }
+                }
+            }
+        }
+
+        private async Task HandleValidSubmit()
+        {
+            busy = true;
+
+            try
+            {
+
+                using (var dbContext = await appSession.DbContextCreate())
+                {
+                    if (_add)
+                    {
+                        /*if (_mesaInscripto == null)
+                        {
+                            throw new Exception("Seleccione una mesa");
+                        }
+                        dbContext.PermisosExamen.Add(_permiso);*/
+                    }
+                    else
+                    {
+                        //si pasa aca es porque quito el permiso y lo agrego devuelta   
+                        /*if (_permiso.Id == 0 && _permisoOrg != null && _permisoOrg.Id != 0)
+                        {
+                            dbContext.PermisosExamen.Remove(_permisoOrg);
+                            if (_mesaInscripto != null)
+                            {
+                                dbContext.PermisosExamen.Add(_permiso);
+                            }
+                        }
+                        else
+                        {
+                            dbContext.PermisosExamen.Update(_permiso);
+                        }*/
+                    }
+
+                    await dbContext.SaveChangesAsync();
+                }
+
+                toastService.ShowSuccess("Grabado");
+                busy = false;
+            }
+            catch (Exception err)
+            {
+                busy = false;
+                if (err.InnerException != null && err.InnerException.Message != "")
+                {
+                    toastService.ShowError(err.InnerException.Message);
+                }
+                else
+                {
+                    toastService.ShowError(err.Message);
+                }
+            }
+
+        }
+
     }
 }
