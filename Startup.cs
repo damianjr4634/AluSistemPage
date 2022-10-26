c5e70064484b194738c7184a3099eb499d173171
damian <rdj4634@gmail.com>
2022-10-25 21:11:11 -0300

    .

--- Startup.cs
+++ Startup.cs
@@ -75,12 +75,14 @@ namespace EsbaBlazorAppAuth
             {
                 app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
+                app.UseForwardedHeaders();
             }
             else
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseForwardedHeaders();
             }
             
             app.UseForwardedHeaders(new ForwardedHeadersOptions
