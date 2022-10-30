7593d027e38e2220de57f642c28cc88e6f8b1fe7
damian <rdj4634@gmail.com>
2022-10-30 15:24:19 -0300

    Revert "."
    This reverts commit e1e4eafecf62850832d348840fb5c591dacfea22.

--- Program.cs
+++ Program.cs
@@ -2,78 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.HttpOverrides;
-using EsbaBlazorAppAuth.Areas.Identity;
-using EsbaBlazorAppAuth.Data;
-using Blazored.Toast;
-using EsbaBlazorAppAuth.Services;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using EsbaBlazorAppAuth.Areas.Identity.Data;
-using System.Net;
+using Microsoft.Extensions.Logging;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-
-builder.Services.AddDbContext<ApplicationDbContext>(ServiceLifetime.Transient);        
-
-builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders()
-    .AddSignInManager<AuthSignInManager<ApplicationUser>>();
-
-builder.Services.AddRazorPages();
-
-builder.Services.AddServerSideBlazor()
-.AddHubOptions(o =>
+namespace EsbaBlazorAppAuth
+{
+    public class Program
     {
-        o.MaximumReceiveMessageSize = 1024000;
-    });
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-builder.Services.AddTransient<IEmailSender, SmtpEmailSender>();
-builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<ApplicationUser>>();
-builder.Services.AddScoped<AppSession>();
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddBlazoredToast();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-  }
-app.UseHttpsRedirection();
-
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-});
-
-ApplicationDbContext.LoadConfig(app.Configuration);
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapControllers();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    //webBuilder.UseUrls("http://*:5000");
+                });
+    }
+}
